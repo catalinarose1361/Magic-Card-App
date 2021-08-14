938ada1dab06b9e28b4fd49b1ba080c55765966a
Catalina Williams <webdevcatalina@gmail.com>
2021-08-14 12:03:27 -0600

    added code that displays a search bar and results to user

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
+// import Forecast from "./components/Forecast/Forecast";
+
+// import Logo from './components/Logo/Logo';
+
+const mtg = require('mtgsdk')
+
 function App() {
+  let [responseObj, setResponseObj] = useState({});
+  let [card, setCard] = useState('');
+
+  function getCards(e) {
+
+    e.preventDefault();
+    setResponseObj({});
+    mtg.card.where({name: card})
+      .then(results => {
+    setResponseObj(results)
+})
+  }
+
+  
+    
+
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <>
+
+    
+
+      <main>
+      <form onSubmit={getCards}>
+
+<input
+
+    type="text"
+    placeholder="Enter Card Name"
+    maxLength="50"
+    value={card}
+   
+    onChange={(e) => setCard(e.target.value)}
+
+/>
+
+
+
+
+
+<button type="submit">Get Cards</button>
+
+</form>
+
+{Object.keys(responseObj).map((item, i) => (
+  <li className="travelcompany-input" key={i}>
+    <span className="input-label">key: {i} Name: {responseObj[i].name}</span>
+  </li>
+))}
+        
+
+      </main>
+
+      <footer>
+
+        Application Developed By <a href="http://www.hirecatalina.com">Catalina Williams</a>
+            
+      </footer>
+
+    </>
+
+  )
+
 }
 
 export default App;
