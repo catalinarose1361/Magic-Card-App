48dac62cd8c45fcc9f8354d8462901f53e31bd0e
Catalina Williams <webdevcatalina@gmail.com>
2021-08-14 12:16:06 -0600

    added card component that displays name and photo

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import './App.css';
 
+import { Grommet, Card, CardHeader, CardBody } from 'grommet';
+
 // import Forecast from "./components/Forecast/Forecast";
 
 // import Logo from './components/Logo/Logo';
@@ -29,7 +31,7 @@ function App() {
 
   return (
 
-    <>
+    <Grommet>
 
     
 
@@ -55,10 +57,20 @@ function App() {
 
 </form>
 
+
+
 {Object.keys(responseObj).map((item, i) => (
-  <li className="travelcompany-input" key={i}>
-    <span className="input-label">key: {i} Name: {responseObj[i].name}</span>
-  </li>
+  <Card key={i} height="medium" width="medium" background="light-1">
+  <CardHeader pad="medium">{responseObj[i].name}</CardHeader>
+  <CardBody pad="medium">
+    <img src={responseObj[i].imageUrl} />
+  </CardBody>
+ 
+</Card>
+
+  // <li className="travelcompany-input" key={i}>
+  //   <span className="input-label">key: {i} Name: {responseObj[i].name}</span>
+  // </li>
 ))}
         
 
@@ -70,7 +82,7 @@ function App() {
             
       </footer>
 
-    </>
+    </Grommet>
 
   )
 
